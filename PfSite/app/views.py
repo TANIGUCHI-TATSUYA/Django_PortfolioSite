e1fd90ccb7e435a92dc27125cb66c21906db01d2
TANISUCHI <ukkome0308@gmail.com>
2023-03-15 17:00:27 +0000

    ディレクトリの修正、CSS追加

--- PfSite/app/views.py
+++ PfSite/app/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.views.generic import View
 from .models import Profile
 # Create your views here.
-class IndeXView(View):
+class IndexView(View):
     def get(self,request,*args,**kwargs):
         profile_data=Profile.objects.all()
         if profile_data.exists():
             profile_data = profile_data.order_by('-id')[0]
-        return render(request,'templates/index.html',{'prfile_data':profile_data})
+        return render(request,'app/index.html',{'profile_data':profile_data})
