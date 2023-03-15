c099f4937c739468990ff348ff49487fd05f6baf
TANISUCHI <ukkome0308@gmail.com>
2023-03-15 02:27:07 +0000

    IIndexViewクラスの作成 プロフィールデータを作成し、Index.htmlへ反映

--- PfSite/app/views.py
+++ PfSite/app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+from .models import Profile
 # Create your views here.
+
+class IndeXView(View):
+    def get(get,request.*args,**kwargs):
+        profile_data=Profile.objects.all()
+        if profile_data.exists():
+            profile_data = profile_data.order_by('-id')[0]
+        return render(request,'templates/index.html',{'prfile_data':profile_data})
