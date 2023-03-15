31294906daeedf7f07689f101d8e33de48b92a20
TANISUCHI <ukkome0308@gmail.com>
2023-03-15 15:21:08 +0000

    class indexview関数の修正

--- PfSite/app/views.py
+++ PfSite/app/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from django.views.generic import View
 from .models import Profile
 # Create your views here.
-
 class IndeXView(View):
-    def get(get,request.*args,**kwargs):
+    def get(self,request,*args,**kwargs):
         profile_data=Profile.objects.all()
         if profile_data.exists():
             profile_data = profile_data.order_by('-id')[0]
