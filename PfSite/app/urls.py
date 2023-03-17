a6827b6f7da028ba22c739f330a49fe3201b3ee7
TANISUCHI <ukkome0308@gmail.com>
2023-03-17 17:06:03 +0000

    DetailViewのhtmls修正及びCSS追加

--- PfSite/app/urls.py
+++ PfSite/app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from app import views
 
 urlpatterns =[
-    path('', views.IndexView.as_view(),name='index')
+    path('', views.IndexView.as_view(),name='index'),
+    path('deatil/<int:pk>/',views.DetailView.as_view(),name='detail'),
+    path('about/',views.AboutView.as_view(),name='about')
 ]
